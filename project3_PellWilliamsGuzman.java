32db7cb15d338e377339f9a115f6b1810cc54e04
JuanHGuzman <82362340+JuanHGuzman@users.noreply.github.com>
2022-10-02 03:55:53 -0400

    Add files via upload

---
+++ project3_PellWilliamsGuzman.java
@@ -0,0 +1,22 @@
+/*
+   Authors: Ashley Pell, Quinten Williams, and Juan Guzman
+   Date: 10/02/2022
+   
+   Write a program (in C++ or Java) that can input from an input file 
+   (named "FDs") that contains a set of functional dependencies (F) and 
+   a set of all attributes (R) for a relation, and output all possible superkeys 
+   for the relation to an output file (named "superkeys").
+   
+*/
+import java.util.*; //Scanner Collection Comp Iter and Set
+import java.io.*; //IOException FileReader FileWriter
+
+public class project3_PellWilliamsGuzman
+{
+
+   public static void main(String[] args) throws IOException
+   {
+   
+   
+   }
+}
