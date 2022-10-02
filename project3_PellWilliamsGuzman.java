a1305f5ca9304dd7a83d0cb160d743ca54bee8d4
JuanHGuzman <82362340+JuanHGuzman@users.noreply.github.com>
2022-10-02 04:09:52 -0400

    Update project3_PellWilliamsGuzman.java

--- project3_PellWilliamsGuzman.java
+++ project3_PellWilliamsGuzman.java
@@ -17,8 +17,27 @@ public class project3_PellWilliamsGuzman
    public static void main(String[] args) throws IOException
    {
       File infile = new File("FDs.txt");
+      Scanner input = new Scanner(infile);
       
       PrintWriter output = new PrinterWriter("superkeys.txt");
-   
+      
+      /*
+         Algorithm:
+         
+         closure = {k}; /*  K is one of the possible superkeys, which 
+         are output of hw1. You must run each entry of the output of hw1 
+         against this algorithm to determine which entry is a superkey
+         
+         do {
+            for(each FD U -> V in FD) do {
+               
+                     if(U is a subset of closure) then
+                        add V to closure;
+            } //  end of the for-loop
+      */
+      input.close();
+      output.close();
+
+      
    }
 }
